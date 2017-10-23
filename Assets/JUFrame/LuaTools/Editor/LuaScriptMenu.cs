da5cdadec4d96594c2c575db5a6dda3a8a8b81e1
王剑虹 <smileking67@gmail.com>
2017-10-23 18:15:12 +0800

    添加创建Lua脚本文件

---
+++ Assets/JUFrame/LuaTools/Editor/LuaScriptMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+namespace JUFrame
+{
+    public class LuaMenu
+    {
+
+        [MenuItem("Assets/Create/Lua Script", false, 80)]
+        public static void CreateNewFile()
+        {
+            ProjectWindowUtil.StartNameEditingIfProjectWindowExists(0,
+                ScriptableObject.CreateInstance<LuaScriptAsset>(),
+                GetSelectedPathOrFallback() + "/New Lua.lua.txt",
+                null,
+                "Assets/JUFrame/LuaTools/Template/template.lua.txt");
+        }
+
+        public static string GetSelectedPathOrFallback()
+        {
+            string path = "Assets";
+            foreach (UnityEngine.Object obj in Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.Assets))
+            {
+                path = AssetDatabase.GetAssetPath(obj);
+                if (!string.IsNullOrEmpty(path) && File.Exists(path))
+                {
+                    path = Path.GetDirectoryName(path);
+                    break;
+                }
+            }
+            return path;
+        }
+    }
+}
+
+
---
+++ Assets/JUFrame/LuaTools/Script/LuaScriptAsset.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.ProjectWindowCallback;
+using System.IO;
+using System.Text.RegularExpressions;
+using UnityEditor;
+using System.Text;
+
+namespace JUFrame
+{
+    public class LuaScriptAsset : EndNameEditAction
+    {
+        public override void Action(int instanceId, string pathName, string resourceFile)
+        {
+            Object o = CreateLuaScriptAssetFromTemplate(pathName, resourceFile);
+            ProjectWindowUtil.ShowCreatedAsset(o);
+        }
+
+        internal static Object CreateLuaScriptAssetFromTemplate(string pathName, string resourceFile)
+        {
+            string fullPath = Path.GetFullPath(pathName);
+            StreamReader streamReader = new StreamReader(resourceFile);
+            string text = streamReader.ReadToEnd();
+            streamReader.Close();
+            string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(pathName);
+            text = Regex.Replace(text, "#NAME#", fileNameWithoutExtension);
+
+            bool encoderShouldEmitUTF8Identifier = true;
+            bool throwOnInvalidBytes = false;
+            UTF8Encoding encoding = new UTF8Encoding(encoderShouldEmitUTF8Identifier, throwOnInvalidBytes);
+            bool append = false;
+            StreamWriter streamWriter = new StreamWriter(fullPath, append, encoding);
+            streamWriter.Write(text);
+            streamWriter.Close();
+            AssetDatabase.ImportAsset(pathName);
+            return AssetDatabase.LoadAssetAtPath(pathName, typeof(Object));
+        }
+    }
+
+}
+
